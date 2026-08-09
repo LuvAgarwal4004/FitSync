6ffc387142dacdaac1af09e965bf3286b46efcff
LuvAgarwal4004 <luvag0707@gmail.com>
2026-08-10 01:38:02 +0530

    Final commits....

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "HITK Stationary",
   description: "Get your style up!",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
